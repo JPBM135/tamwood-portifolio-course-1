d7564747ec32760217fcb6c6bf0fd63e63374b03
JPBM135 <jpedrobm0@gmail.com>
2024-02-14 16:30:07 -0800

    refactor(faq): improve legibility

--- src/scripts/pages/faq.js
+++ src/scripts/pages/faq.js
@@ -1,13 +1,14 @@
+const OPEN_CLASS = "main__accordion__body-wrapper--open";
+
 function toggle(index) {
   const elementsToClose = document.querySelectorAll(
     `.main__accordion__body-wrapper:not(#accordion-toggle-${index})`
   );
 
   for (const element of elementsToClose) {
-    element.classList.remove('main__accordion__body-wrapper--open');
+    element.classList.remove(OPEN_CLASS);
   }
 
   const toggleElement = document.getElementById(`accordion-toggle-${index}`);
-
-  toggleElement?.classList.toggle('main__accordion__body-wrapper--open');
+  toggleElement?.classList.toggle(OPEN_CLASS);
 }
